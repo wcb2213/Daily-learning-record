7bf25251c394f995acd02a9625277fbd684194a9
Wu Chenbang <wcb2213@163.com>
2019-03-25 20:56:49 +0800

    update 42S

--- jianzhioffer/42S.py
+++ jianzhioffer/42S.py
@@ -15,6 +15,6 @@ class Solution:
                 if array[i] + array[j] == tsum:
                     res.append(array[i])
                     res.append(array[j])
-                    # res.append([array[i], array[j]]) # 不一样
+                    # res.append([array[i], array[j]]) # 不一样？
                     return res
         return []
