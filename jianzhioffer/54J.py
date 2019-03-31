0cca79142d963c12f00fa7f2da49f6246aec72dd
Wu Chenbang <wcb2213@163.com>
2019-03-31 17:33:19 +0800

    add 54

---
+++ jianzhioffer/54J.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env/ python
+# -*- coding:utf-8 -*-
+# Created by: Vanish
+# Created on: 2019/3/31
+
+
+class Solution:
+    # 返回对应char
+    def __init__(self):
+        self.s = ''
+        self.dict = {}
+    def FirstAppearingOnce(self):
+        # write code here
+        for i in self.s:
+            if self.dict[i] == 1:
+                return i
+        return '#'
+    # 创建字典给每个字符计数 char为单个字符
+    def Insert(self, char):
+        # write code here
+        self.s += char
+        if char in self.dict:
+            self.dict[char] += 1
+        else:
+            self.dict[char] = 1
