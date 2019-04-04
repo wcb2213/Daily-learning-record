18348112dd81080206751e5d0d705bcd734dace3
Wu Chenbang <wcb2213@163.com>
2019-04-04 20:35:51 +0800

    add 63

---
+++ jianzhioffer/63mine.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env/ python
+# -*- coding:utf-8 -*-
+# Created by: Vanish
+# Created on: 2019/4/4
+
+
+class Solution:
+    def __init__(self):
+        self.data = []
+    def Insert(self, num):
+        # write code here
+        self.data.append(num)
+        self.data.sort()
+    def GetMedian(self, data):
+        # write code here
+        n = len(self.data)
+        if n % 2 == 1:
+            return self.data[(n-1)/2]
+        else:
+            return (self.data[n/2] + self.data[n/2-1])/2.0
