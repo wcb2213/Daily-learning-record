cdcb9b05c77d80b20c119cae26ff207045b98b6d
Wu Chenbang <wcb2213@163.com>
2019-04-18 22:38:24 +0800

    update 13 列表生成式

--- jianzhioffer/python/13S.py
+++ jianzhioffer/python/13S.py
@@ -3,6 +3,8 @@
 # Created by Vanish at 2019/2/24
 
 
+
+# 26ms
 class Solution:
     def reOrderArray(self, array):
         # write code here
@@ -12,14 +14,4 @@ class Solution:
                 s2.append(i)
             else:
                 s1.append(i)
-        s1.extend(s2)
-        return s1
-
-# 第一种
-# s1, s2 = [], []
-# s2.append(1)
-# s1.extend(s2)
-# print(s1)
-# 第二种
-# s3 = s1 + s2
-# print(s3)
+        return s1+s2
