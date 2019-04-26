c3b996b362a9c53160d3b285063baf34265bd966
Wu Chenbang <wcb2213@163.com>
2019-04-26 22:00:06 +0800

    update 4/26 Attention

--- jianzhioffer/python/52J_正则表达式 及 re模块_A.py
+++ jianzhioffer/python/52J_正则表达式 及 re模块_A.py
@@ -10,8 +10,15 @@ class Solution:
     def match(self, s, pattern):
         # write code here
         import re
+        # 返回所有s中符合正则式pattern的字符串 k为list
         k = re.findall(pattern, s)
         if s in k:
             return True
         else:
             return False
+
+if __name__ == '__main__':
+    s = 'aaa'
+    pattern = "ab*ac*a"
+    a = Solution()
+    print(a.match(s,pattern))
